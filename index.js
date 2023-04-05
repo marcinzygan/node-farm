298021410d5e97de5625ff9d8bbc07cde91823de
marcinzygan <soul@o2.pl>
2023-04-05 19:11:28 +0200

    asynschronus way in node

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const fs = require("fs");
+// blocking synchronus way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+// const textOut = `Marcin is the best chef ever. NOT`;
+// fs.writeFileSync("./txt/output.txt", textOut);
 
-const textOut = `Marcin is the best chef ever. NOT`;
-fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("file written");
 
-console.log("file written");
+//  non blocking asynchronus way
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  console.log(data);
+});
+
+console.log("This will log first !");
