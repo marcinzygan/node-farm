4e4796a85c0940d21fad79fff541ffe8b326e076
marcinzygan <soul@o2.pl>
2023-04-05 17:05:03 +0200

    write and read file in node

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const fs = require("fs");
-const hello = "hello world";
-console.log(hello);
+
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
+
+const textOut = `Marcin is the best chef ever. NOT`;
+fs.writeFileSync("./txt/output.txt", textOut);
+
+console.log("file written");
